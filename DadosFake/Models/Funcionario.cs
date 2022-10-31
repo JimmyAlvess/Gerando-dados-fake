ffe1e9ecca91078fcee015dcb696436b88bb4442
Matheus Alves <matheus.180960@gmail.com>
2022-10-31 11:51:53 -0300

    Dados fakes

---
+++ DadosFake/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DadosFake
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string? Cpf { get; set; }
+        public string? NomeCompelto { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Contato { get; set; }
+
+    }
+}
---
+++ DadosFake/Program.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace DadosFake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionarioFake = new Faker<Funcionario>()
+                .RuleFor(f => f.Cpf, d => new Person("pt_BR").Cpf(false))
+                .RuleFor(f => f.NomeCompelto, d => d.Name.FullName())
+                .RuleFor(f => f.DataNascimento, d => d.Date.Past(50))
+                .RuleFor(f => f.Contato, d => d.Phone.PhoneNumber());
+
+            var funcinario = funcionarioFake.Generate(100);
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            string jsonString = JsonSerializer.Serialize(funcinario, options);
+
+            Console.WriteLine(jsonString);
+
+            //using (var connection = new SqlConnection("Data Source = Jimmy\\SQLEXPRESS; Initial Catalog = Funcioanrio; Integrated Security = True"))
+            //{
+            //    connection.Open();
+            //    connection.Insert(funcinario);
+            //}
+        }
+
+    }
+}
